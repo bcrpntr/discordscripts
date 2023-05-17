378141f89248e05800b49dc4e9b5f47b60fa1ff6
bcrpntr <44307215+bcrpntr@users.noreply.github.com>
2023-05-17 16:02:15 -0500

    Delete calculate_circle_area.py

--- scripts/calculate_circle_area.py
+++
@@ -1,8 +0,0 @@
-# This script calculates the area of a circle
-
-import math # import the math module
-
-def calculate_circle_area(radius): # define the function to calculate area of a circle
-    return math.pi * radius ** 2
-
-print(calculate_circle_area(5)) # call the function with a radius of 5 and print the result
