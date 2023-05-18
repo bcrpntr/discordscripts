94caac91db468a14481c792a99a57d14bfe59997
bcrpntr <44307215+bcrpntr@users.noreply.github.com>
2023-05-18 14:16:53 -0500

    Add circle_area_calculator.py

---
+++ circle_area_calculator.py
@@ -0,0 +1,8 @@
+# This script calculates the area of a circle.
+
+# The user enters the radius of the circle.
+
+# The area of the circle is calculated using the formula:
+# area = pi * (radius ** 2)
+
+# The area of the circle is then printed.
